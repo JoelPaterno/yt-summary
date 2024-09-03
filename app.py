b663b3538e1d98eb758e274517be4888972eea7c
joelpaterno <joelpaterno1@gmail.com>
2024-09-03 21:46:17 +1000

    added beatiful soup to clean html, and trying to get selenium to open the transcript ot be read

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from apitest import get_summary
+from webscraper import scrape_website, extract_body_content, clean_body_content
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -10,7 +11,10 @@ def home():
         url = request.form["url"]
         length = request.form["length"]
         summary = get_summary(url=url, length=length)
-        return render_template("summary.html", url=url, summary=summary)
+        webpage = scrape_website(url)
+        webpage = extract_body_content(webpage)
+        webpage = clean_body_content(webpage)
+        return render_template("summary.html", url=url, summary=summary, html=webpage)
     else:
         return render_template("index.html")
 
--- webscraper.py
+++ webscraper.py
@@ -1,16 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 import time
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+def scrape_website(website) -> None:
+    print("launching chrome browser...")
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
-driver.get("https://www.youtube.com/watch?v=NB8OceGZGjA")
+    try:
+        driver.get(website)
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'expand'))
+        )
+        element.click()
+        element2 = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'button-container'))
+        )
+        driver.execute_script("arguments[0].click();", element2)
+        time.sleep(5)
+        html = driver.page_source
+        return html
+    finally:
+        driver.quit()
 
-time.sleep(5)
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
 
-input_element = driver.find_element(By.CLASS_NAME, "style-scope ytd-video-description-transcript-section-renderer")   
-input_element.click()
-
-driver.quit()
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+    cleaned_content = soup.get_text(separator="\n")
+    #cleaned_contant = "\n".join()
+    return cleaned_content
